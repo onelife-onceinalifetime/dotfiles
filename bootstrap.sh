84def19c174385799e3f6f901b3f4a08d6b98e6f
Max <minkyu2216@gmail.com>
2026-06-29 19:09:54 +0900

    Modified bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,7 @@ doIt() {
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "windows-terminal/" \
+		--exclude "packages.sh" \
 		-avh --no-perms . ~
 	source ~/.bashrc
 }
@@ -13,7 +14,7 @@ doIt() {
 if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
 	doIt
 else
-	read -p "홈의 기존 파일을 덮어쓸 수 있습니다. 진행할까요? (y/n) " -n 1
+	read -p "This can overwrite your original files. Do you want to continue? (y/n) " -n 1
 	echo ""
 	[[ $REPLY =~ ^[Yy]$ ]] && doIt
 fi
