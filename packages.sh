ac62a039f867cb79f7a12acbebcd748b7a9ac27b
Max <minkyu2216@gmail.com>
2026-07-03 16:07:50 +0900

    modified packages.sh

--- packages.sh
+++ packages.sh
@@ -3,8 +3,8 @@ set -e
 sudo apt update
 sudo apt install -y \
 	git gh tmux rsync curl unzip \
-	neovim \
-	nodejs npm \
+	neovim nano \
+	gcc gdb openssh \
 	python3-pip python3-venv \
 	ripgrep zoxide tealdeer eza
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
