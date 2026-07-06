59e5e30bbc46c240f2b9b744adc553630b626770
Max <minkyu2216@gmail.com>
2026-07-06 20:19:28 +0900

    Changed ssh install command

--- packages.sh
+++ packages.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install -y \
 	git gh tmux rsync curl unzip \
 	neovim nano \
-	gcc gdb openssh \
+	gcc gdb openssh-client openssh-server \
 	python3-pip python3-venv \
 	ripgrep zoxide tealdeer eza
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
